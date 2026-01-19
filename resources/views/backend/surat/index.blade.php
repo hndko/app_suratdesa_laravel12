affbc3be78dd32da7f0537635519e633fd67da28
hndko <hd.doko22@gmail.com>
2026-01-19 16:53:20 +0700

    feat: Add new index views for penduduk and surat management.

--- resources/views/backend/surat/index.blade.php
+++ resources/views/backend/surat/index.blade.php
@@ -7,6 +7,14 @@
 <link href="{{ asset('assets/plugins/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/plugins/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css') }}"
     rel="stylesheet" />
+<style>
+    /* Force wrap on mobile detail view */
+    table.dataTable>tbody>tr.child span.dtr-data {
+        white-space: normal !important;
+        word-wrap: break-word;
+        word-break: break-all;
+    }
+</style>
 @endpush
 
 @section('content')
@@ -85,7 +93,7 @@
 <script src="{{ asset('assets/plugins/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
 <script>
     $('#datatableDefault').DataTable({
-        dom: "<'row mb-3'<'col-md-4 mb-3 mb-md-0'l><'col-md-8'<'d-flex justify-content-center justify-content-md-end'f<'ms-2'B>>>>t<'row align-items-center mt-3'<'mr-auto col-md-6'i><'mb-0 col-md-6'p>>",
+        dom: "<'row mb-3'<'col-12 col-md-6 d-flex justify-content-center justify-content-md-start mb-2 mb-md-0'l><'col-12 col-md-6 d-flex justify-content-center justify-content-md-end align-items-center gap-2 flex-wrap'fB>>t<'row align-items-center mt-3'<'mr-auto col-md-6'i><'mb-0 col-md-6'p>>",
         lengthMenu: [ 10, 20, 30, 40, 50 ],
         responsive: true,
         order: [[ 4, "desc" ]], // Keep sort by date
